3cee55a038da9fa8cc1375ddbabba62090981df7
Aaron lawrence <91675986+AaronNeonDigital@users.noreply.github.com>
2023-06-29 16:40:55 +0100

    rrr

--- js/TerritoryWarWatcher/index.js
+++ js/TerritoryWarWatcher/index.js
@@ -1,8 +1,4 @@
 // Define an object with a method
-const tt = {
-    build: function() {
-        console.log("Hello from myMethod!");
-    }
+const build = () => {
+    console.log("Hello from myMethod!");
 };
-
-export default tt;
