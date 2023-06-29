c00c8cf4f49d425705369c2317ec45757ae8371a
Aaron lawrence <91675986+AaronNeonDigital@users.noreply.github.com>
2023-06-29 15:31:22 +0100

    Update index.js

--- js/TerritoryWarWatcher/index.js
+++ js/TerritoryWarWatcher/index.js
@@ -1,1 +1,9 @@
-// Index js file
+// Define an object with a method
+const tt = {
+    build: function() {
+        console.log("Hello from myMethod!");
+    }
+};
+
+// Export the object to make it accessible outside the file
+export default tt;
